e40da274ea356e7fe17144cde62b5cf41f5d0485
Satyam Singhal <satyamsinghal368@gmail.com>
2024-12-18 11:38:07 +0530

    Fix Tailwind config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      tailwindcss: {
+        config: "./tailwind.config.cjs", // or `tailwind.config.ts`
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
